b9a3328ae0d97be30ee30b7e176bd72d6ab63254
YiYing Chiang <148739232+elena1211@users.noreply.github.com>
2024-10-01 11:39:15 +0100

    delete

--- Hunt for Red October.py
+++
@@ -1,7 +0,0 @@
-### the scrambled message to Red October
-scrambled="QNTSINUAYGTOTPENAOGCSYDBBLTHRYTOYSWPMWTBESRNAEETBHTNTBGOAPTMNETHMIROCYCGULAYEGEWA"
-scrambled_list = list(scrambled)
-scrambled_list.reverse()
-list1 = scrambled_list[1::3]
-sentence1 = ''.join(list1)
-print(sentence1)
